50ede675272203b25a49c1872b86fb9d75c1f54b
sumfish <f74036190@mail.ncku.edu.tw>
2019-11-27 23:16:24 +0800

    add spectrogram filter(log) & check transpose(128,T)

--- src/dataloader.py
+++ src/dataloader.py
@@ -14,16 +14,19 @@ class Style_Dataset(Dataset):
         self.data=load_npy(audio_data)
         self.label=load_npy(label_data)
         self.avgv=load_npy('avg.npy')
-        self.stdv=load_npy('std.npy')
+        self.stdv=load_npy('std.npy') 
         #self.transform = transform
 
     def __getitem__(self,index):
         audio=self.data[index]
         ## normaliztion
-        audio = np.transpose(np.log(1+10000*audio)) ####?????????
+        print(audio.shape) #(128,130)
+        audio = np.transpose(np.log(1+10000*audio)) #### filter, spe(128,T)->(T,128))
         audio=(audio-self.avgv)*self.stdv
+        audio=np.transpose(audio)
         audio_tensor=Tensor(audio).view(1,audio.shape[0],audio.shape[1]).float()
-        
+        #print(audio_tensor.shape) # torch.Size([1, 128, 130])
+
         classifi=int(self.label[index])
 
         return [audio_tensor,classifi]
@@ -31,7 +34,7 @@ class Style_Dataset(Dataset):
     def __len__(self):
         return len(self.label)
 
-data=Style_Dataset('data.npy','label.npy').__getitem__(200)
+data=Style_Dataset('data.npy','label.npy').__getitem__(1)
 #print(Style_Dataset('data.npy','label.npy').__len__())
 #print(data[0].size()[2:])
 #print(data[0])
